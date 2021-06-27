0d8b65b05c0d4dd16306a06c775d37038d1eaf2f
halyna-ala <77680693+halyna-ala@users.noreply.github.com>
2021-06-27 08:21:04 +0200

    theme-save

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,37 +11,62 @@ const eventBoard = document.querySelector('.js-menu');
 eventBoard.innerHTML = eventsTemplate(upcomingEvents);
 
 // window.onload = () => {
-//         const menu = document.getElementById("js-menu");
-//         menu.innerHTML = eventsTemplate(upcomingEvents);
-//     };
+  //         const menu = document.getElementById("js-menu");
+  //         menu.innerHTML = eventsTemplate(upcomingEvents);
+  //     };
+  
+  const Theme = {
+      LIGHT: 'light-theme',
+      DARK: 'dark-theme',
+    };
 
 const bodyEl = document.querySelector('body');
-
 const themeSwitch = document.querySelector('#theme-switch-toggle'); 
 
-themeSwitch.addEventListener('change', onThemeSwitch);
-
-    const Theme = {
-        LIGHT: 'light-theme',
-        DARK: 'dark-theme',
-      };
-
-bodyEl.classList.add(Theme.LIGHT);
-
-function onThemeSwitch(evt) {
-
-const isLightTheme = themeSwitch.checked;
+themeSwitch.addEventListener('change', onClickThemeSwitch);
 
 
-  if (isLightTheme) {
-    bodyEl.classList .replace(Theme.LIGHT, Theme.DARK);
-} else bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
 
-//   isLightTheme = !isLightTheme;
+function onClickThemeSwitch() {
+  if (bodyEl.classList.value === Theme.LIGHT) {
+      bodyEl.classList.toggle(Theme.DARK)
+      bodyEl.classList.toggle(Theme.LIGHT)
 
-localStorage.setItem('checked-Value', isLightTheme);
-    const savedChecked = localStorage.getItem('checked-Value');
-    console.log(savedChecked);
-    
+      localStorage.setItem('theme-type', bodyEl.classList.value)
+  } else {
+      bodyEl.classList.toggle(Theme.DARK)
+      bodyEl.classList.toggle(Theme.LIGHT)
+
+      localStorage.setItem('theme-type', bodyEl.classList.value)
+  }
 }
 
+
+if (localStorage.getItem('theme-type') === Theme.DARK) {
+  localStorage.setItem('theme-type', Theme.DARK)
+  // themeSwitch.setAttribute('chacked', true)
+  bodyEl.classList.add(Theme.DARK)
+} else {
+  bodyEl.classList.add(Theme.LIGHT) 
+}
+
+
+
+// bodyEl.classList.add(Theme.LIGHT);
+
+// function onThemeSwitch(evt) {
+
+// const isLightTheme = themeSwitch.checked;
+
+
+//   if (isLightTheme) {
+//     bodyEl.classList .replace(Theme.LIGHT, Theme.DARK);
+// } else bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
+
+// //   isLightTheme = !isLightTheme;
+
+// localStorage.setItem('checked-Value', isLightTheme);
+//     const savedChecked = localStorage.getItem('checked-Value');
+//     console.log(savedChecked);
+    
+// }
